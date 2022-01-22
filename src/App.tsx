8e2e47333ff7d8309bdf8fe1585ee146dce51131
TheBestDon <donatas_bakanas@yahoo.com>
2022-01-23 01:28:34 +0200

    feat: dark theme added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { PageWrapper } from './state'
 import Breeds from './components/Cats/Breeds'
 import styled from 'styled-components'
+import Toggle from './components/Toggle'
 
 const AppStyles = styled.div`
   margin: 50px auto;
-  width: 380px;
+  width: 50vw;
+  min-height: var(--window-height);
 
   .container {
-    background-color: #1d1e26;
-    border: 4px solid #9580ff;
+    background-color: var(--clr-bg-2);
+    border: 4px solid var(--clr-secondary);
     border-radius: 6px;
     padding: 25px;
+  }
+
+  @media (max-width: 768px) {
+    width: 90vw;
   }
 `
 
@@ -23,7 +29,7 @@ function App() {
         <PageWrapper>
           <div className="container">
             <header>
-              <Link to="/">Home</Link>
+              <Toggle />
             </header>
             <Routes>
               <Route path="/*" element={<Breeds />} />
